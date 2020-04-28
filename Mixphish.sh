df7296d2082e39f0f5efcf32e02f2eb7d5d57443
Sasori <63850356+Sonata7@users.noreply.github.com>
2020-04-28 08:14:21 +0000

    Create Mixphish.sh
    Just for practice

---
+++ Mixphish.sh
@@ -0,0 +1,39 @@
+#!usr/bin/bash
+$Green
+figlet -f big Sonata7
+echo "I created this tool for easy install all phishing too "
+echo "now type "start" to install the tool"
+
+read action
+
+for loop in {1..100};do
+echo "wait a second$loop%"
+done
+
+
+
+echo "wait a second "
+
+echo "................................................................"
+
+echo " ================++====="
+
+
+git clone https://github.com/thelinuxchoice/shellphish
+
+pkg install openssh
+
+pip2 install -r requirements.txt
+
+git clone https://github.com/UndeadSec/SocialFish.git
+
+pip2 install -r requirements.txt
+
+git clone https://github.com/evait-security/weeman
+
+for loop in {1..100};do
+echo "preparing tools $loop% "
+done
+
+echo "Every thing is ready now "
+echo " Have a nice day "
